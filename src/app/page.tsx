6d8ad7fa8685703d315e3a0799ff2bca1e43fafb
Gabriel Lulu <glulu4444@gmail.com>
2025-05-05 10:55:55 -0400

    eornds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,34 @@
 import CTA from "@/components/landing/CTA";
 import Hero from "@/components/landing/Hero";
 import IdealCanidate from "@/components/landing/IdealCanidate";
-import Testimonials from "@/components/TestimonialCard";
-import TrustedBy from "@/components/TrustedBy";
 import VolunteerStories from "@/components/VolunteerStories";
 
 
 
-// const images = [
-//   {
-//     id: 1,
-//     title: 'Motivation',
-//     description:`
-//     EM in Israel is a young specialty with tremendous growth potential and clinical, educational and academic support needs that preceded October 7th. After October 7th, over 350,000 reservists were called up to serve and protect Israel, including a significant number of EM providers, leaving several emergency departments across the country further understaffed. 
-//     `,
-//     imageUrl: "/images/home/img1.jpg",
-//   },
-//   {
-//     id: 2,
-//     title: 'Join the Movement',
-//     description:
-//       'Switch to renewable energy and reduce your carbon footprint. Solar energy is clean, sustainable, and the perfect choice for environmentally conscious individuals. Let’s build a greener future together.',
-//     imageUrl: "/images/home/img2.jpg",  },
-//   {
-//     id: 3,
-//     title: 'Increase Your Property Value',
-//     description:
-//       'Solar panels not only save energy but also enhance your property’s market value. Invest in a long-term energy solution that adds tangible value to your home or business.',
-//     imageUrl: "/images/home/img3.jpg",  },
-// ];
+const images = [
+  {
+    id: 1,
+    title: 'Motivation',
+    description:`
+    EM in Israel is a young specialty with tremendous growth potential and clinical, educational and academic support needs that preceded October 7th. After October 7th, over 350,000 reservists were called up to serve and protect Israel, including a significant number of EM providers, leaving several emergency departments across the country further understaffed. 
+    `,
+    imageUrl: "/images/home/img1.jpg",
+  },
+  {
+    id: 2,
+    title: 'Join the Movement',
+    description:
+      'Switch to renewable energy and reduce your carbon footprint. Solar energy is clean, sustainable, and the perfect choice for environmentally conscious individuals. Let’s build a greener future together.',
+    imageUrl: "/images/home/img2.jpg",  },
+  {
+    id: 3,
+    title: 'Increase Your Property Value',
+    description:
+      'Solar panels not only save energy but also enhance your property’s market value. Invest in a long-term energy solution that adds tangible value to your home or business.',
+    imageUrl: "/images/home/img3.jpg",  },
+];
 
-// export type ThreeImageArrayType = typeof images[number];
+export type ThreeImageArrayType = typeof images[number];
 
 const Page = () => {
   return (
